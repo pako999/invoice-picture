e00aebc2b52e1f55ff2aa49e0aab2fe8e44bfa8b
Patrik <125508378+pako999@users.noreply.github.com>
2026-09-07 14:08:10 +0200

    Render processing history below invoice review

--- app/invoice-review/[id]/page.tsx
+++ app/invoice-review/[id]/page.tsx
@@ -1,6 +1,13 @@
 import { InvoiceReviewClient } from "./review-client";
+import { ProcessingHistory } from "./processing-history";
 
 export default async function InvoiceReviewDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-  return <InvoiceReviewClient documentId={Number(id)} />;
+  const documentId = Number(id);
+  return (
+    <>
+      <InvoiceReviewClient documentId={documentId} />
+      <ProcessingHistory documentId={documentId} />
+    </>
+  );
 }
