aaa53f39fa3f1be118cb6e392de02e9006c31e85
Patrik <125508378+pako999@users.noreply.github.com>
2026-09-17 13:22:20 +0200

    Fix invoice approval button visibility

---
+++ app/invoice-review/[id]/layout.tsx
@@ -0,0 +1,5 @@
+import "./review-route.css";
+
+export default function InvoiceReviewDetailLayout({ children }: { children: React.ReactNode }) {
+  return <div className="invoice-review-route">{children}</div>;
+}
